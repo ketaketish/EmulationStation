d025b10cd7970de2cc3d7393e8e84d575b919466
ketaketish <38956824+ketaketish@users.noreply.github.com>
2019-02-18 00:42:30 -0500

    Update ViewController.cpp

--- es-app/src/views/ViewController.cpp
+++ es-app/src/views/ViewController.cpp
@@ -437,7 +437,7 @@ void ViewController::preload()
 		{
 			i++;
 			char buffer[100];
-			sprintf (buffer, "Loading '%s' (%d/%d)\nCustom Image by Doctor X",
+			sprintf (buffer, "Loading '%s' (%d/%d)",
 				(*it)->getFullName().c_str(), i, SystemData::sSystemVector.size());
 			mWindow->renderLoadingScreen(std::string(buffer));
 		}
