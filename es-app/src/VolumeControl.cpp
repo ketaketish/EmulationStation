cdb9d24a30c3594f71626129dfc757b61d6939e9
ketaketish <38956824+ketaketish@users.noreply.github.com>
2019-02-18 20:52:01 -0500

    Add files via upload

--- es-app/src/VolumeControl.cpp
+++ es-app/src/VolumeControl.cpp
@@ -1,6 +1,5 @@
 #include "VolumeControl.h"
 
-#include "math/Misc.h"
 #include "Log.h"
 #include "Settings.h"
 #ifdef WIN32
@@ -8,7 +7,7 @@
 #endif
 
 #if defined(__linux__)
-    #if defined(_RPI_) || defined(_VERO4K_)
+    #ifdef _RPI_
         const char * VolumeControl::mixerName = "PCM";
     #else
     	const char * VolumeControl::mixerName = "Master";
@@ -22,7 +21,7 @@ std::weak_ptr<VolumeControl> VolumeControl::sInstance;
 VolumeControl::VolumeControl()
 	: originalVolume(0), internalVolume(0)
 #if defined (__APPLE__)
-	#error TODO: Not implemented for MacOS yet!!!
+    #error TODO: Not implemented for MacOS yet!!!
 #elif defined(__linux__)
 	, mixerIndex(0), mixerHandle(nullptr), mixerElem(nullptr), mixerSelemId(nullptr)
 #elif defined(WIN32) || defined(_WIN32)
@@ -36,16 +35,15 @@ VolumeControl::VolumeControl()
 }
 
 VolumeControl::VolumeControl(const VolumeControl & right):
-	originalVolume(0), internalVolume(0)
+  originalVolume(0), internalVolume(0)
 #if defined (__APPLE__)
-	#error TODO: Not implemented for MacOS yet!!!
+    #error TODO: Not implemented for MacOS yet!!!
 #elif defined(__linux__)
 	, mixerIndex(0), mixerHandle(nullptr), mixerElem(nullptr), mixerSelemId(nullptr)
 #elif defined(WIN32) || defined(_WIN32)
 	, mixerHandle(nullptr), endpointVolume(nullptr)
 #endif
 {
-	(void)right;
 	sInstance = right.sInstance;
 }
 
@@ -86,9 +84,9 @@ void VolumeControl::init()
 	//try to open mixer device
 	if (mixerHandle == nullptr)
 	{
-		// Allow users to override the AudioCard and MixerName in es_settings.cfg
-		mixerCard = Settings::getInstance()->getString("AudioCard").c_str();
+		#ifdef _RPI_
 		mixerName = Settings::getInstance()->getString("AudioDevice").c_str();
+		#endif
 
 		snd_mixer_selem_id_alloca(&mixerSelemId);
 		//sets simple-mixer index and name
@@ -256,7 +254,7 @@ int VolumeControl::getVolume() const
 	int volume = 0;
 
 #if defined (__APPLE__)
-	#error TODO: Not implemented for MacOS yet!!!
+    #error TODO: Not implemented for MacOS yet!!!
 #elif defined(__linux__)
 	if (mixerElem != nullptr)
 	{
@@ -301,7 +299,7 @@ int VolumeControl::getVolume() const
 		mixerControlDetails.cbDetails = sizeof(MIXERCONTROLDETAILS_UNSIGNED);
 		if (mixerGetControlDetails((HMIXEROBJ)mixerHandle, &mixerControlDetails, MIXER_GETCONTROLDETAILSF_VALUE) == MMSYSERR_NOERROR) 
 		{
-			volume = (int)Math::round((value.dwValue * 100) / 65535.0f);
+			volume = (uint8_t)round((value.dwValue * 100) / 65535);
 		}
 		else
 		{
@@ -314,7 +312,7 @@ int VolumeControl::getVolume() const
 		float floatVolume = 0.0f; //0-1
 		if (endpointVolume->GetMasterVolumeLevelScalar(&floatVolume) == S_OK)
 		{
-			volume = (int)Math::round(floatVolume * 100.0f);
+			volume = (uint8_t)round(floatVolume * 100.0f);
 			LOG(LogInfo) << " getting volume as " << volume << " ( from float " << floatVolume << ")";
 		}
 		else
@@ -350,7 +348,7 @@ void VolumeControl::setVolume(int volume)
 	//store values in internal variables
 	internalVolume = volume;
 #if defined (__APPLE__)
-	#error TODO: Not implemented for MacOS yet!!!
+    #error TODO: Not implemented for MacOS yet!!!
 #elif defined(__linux__)
 	if (mixerElem != nullptr)
 	{
--- es-app/src/views/ViewController.h
+++ es-app/src/views/ViewController.h
@@ -11,6 +11,8 @@ class IGameListView;
 class SystemData;
 class SystemView;
 
+const std::vector<std::string> UIModes = { "Full", "Kiosk", "Kid" };
+
 // Used to smoothly transition the camera between multiple views (e.g. from system to system, from gamelist to gamelist).
 class ViewController : public GuiComponent
 {
@@ -30,13 +32,17 @@ public:
 	inline void reloadGameListView(SystemData* system, bool reloadTheme = false) { reloadGameListView(getGameListView(system).get(), reloadTheme); }
 	void reloadAll(); // Reload everything with a theme.  Used when the "ThemeSet" setting changes.
 
+	void monitorUIMode();
+	inline std::vector<std::string> getUIModes() { return UIModes; };
+	bool isUIModeFull();
+	bool isUIModeKid();
+
 	// Navigation.
 	void goToNextGameList();
 	void goToPrevGameList();
 	void goToGameList(SystemData* system);
 	void goToSystemView(SystemData* system);
 	void goToStart();
-	void ReloadAndGoToStart();
 
 	void onFileChanged(FileData* file, FileChangeType change);
 
@@ -61,8 +67,8 @@ public:
 		AUTOMATIC,
 		BASIC,
 		DETAILED,
-		GRID,
 		VIDEO
+		// GRID TODO!
 	};
 
 	struct State
@@ -101,6 +107,7 @@ private:
 	bool mLockInput;
 
 	State mState;
+	std::string mCurUIMode;
 };
 
 #endif // ES_APP_VIEWS_VIEW_CONTROLLER_H
